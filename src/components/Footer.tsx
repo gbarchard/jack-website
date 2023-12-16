a17f1bd380b400a33d19dee4bd666ba51a770a10
Grant Barchard <grant.barchard@gmail.com>
2023-12-16 16:51:19 -0500

    kofi link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
 import { Footer } from 'flowbite-react'
 import { Instagram, Twitter } from 'react-bootstrap-icons'
+import Kofi from './Kofi'
 
 export default function App() {
   return (
-    <Footer className="rounded-none shadow-none md:justify-end bg-gray-100" container>
+    <Footer
+      className="rounded-none shadow-none md:justify-end bg-gray-100"
+      container
+    >
       <div className="mt-4 flex space-x-6 sm:mt-0">
         <Footer.Icon
           className="hover:text-cyan-700"
@@ -16,6 +20,12 @@ export default function App() {
           href="https://twitter.com/TeskeBoyArt/"
           target="blank"
           icon={Twitter}
+        />
+        <Footer.Icon
+          className="hover:text-cyan-700"
+          href="https://ko-fi.com/teskeboy"
+          target="blank"
+          icon={Kofi}
         />
       </div>
     </Footer>
